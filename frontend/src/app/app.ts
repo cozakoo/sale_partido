02ba8e5f70fca3de21dc1c002c8d4e2aa53ce981
fabianvm <vidalfabian013@gmail.com>
2026-05-03 21:34:43 -0300

    chore: pom fix and full stack example

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,12 +1,42 @@
-import { Component, signal } from '@angular/core';
+import { Component, signal, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+
+interface HelloWorld {
+  uuid: string;
+  text: string;
+  source: string;
+}
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
-export class App {
+export class App implements OnInit {
   protected readonly title = signal('frontend');
+  textInput = '';
+  helloWorlds: HelloWorld[] = [];
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.fetchHelloWorlds();
+  }
+
+  fetchHelloWorlds() {
+    this.http.get<HelloWorld[]>('http://localhost:8080/').subscribe((data) => {
+      this.helloWorlds = data;
+    });
+  }
+
+  submitText() {
+    if (!this.textInput.trim()) return;
+    this.http.post('http://localhost:8080/', { text: this.textInput }).subscribe(() => {
+      this.textInput = '';
+      this.fetchHelloWorlds();
+    });
+  }
 }
