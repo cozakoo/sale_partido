b3b387a8d6820a4b221d9ed615dfeb2d6fa606f2
fabianvm <vidalfabian013@gmail.com>
2026-05-08 21:42:32 -0300

    chore: datafaker añadido y probado

--- backend/src/main/java/io/github/salepartido/api/devtools/canchas/DevCanchaController.java
+++ backend/src/main/java/io/github/salepartido/api/devtools/canchas/DevCanchaController.java
@@ -1,13 +1,19 @@
 package io.github.salepartido.api.devtools.canchas;
 
+import java.util.Collection;
+
 import org.springframework.context.annotation.Profile;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@RequestMapping("/dev")
+import io.github.salepartido.api.domain.canchas.model.ConfiguracionDia;
+
 @Profile("dev")
+@RestController
+@RequestMapping("/dev-canchas")
 public class DevCanchaController {
     
     private final SeedService seedService;
@@ -16,8 +22,15 @@ public class DevCanchaController {
         this.seedService = seedService;
     }
 
+    @GetMapping("/preview/{count}")
+    public Collection<ConfiguracionDia> preview(
+        @PathVariable("count") int count
+    ) {
+        return seedService.seed(count);
+    }
+
     @PostMapping("/seed")
     public void seed() {
-        seedService.seed();
+        // poblar la db
     }
 }
--- backend/src/main/java/io/github/salepartido/api/devtools/canchas/SeedService.java
+++ backend/src/main/java/io/github/salepartido/api/devtools/canchas/SeedService.java
@@ -1,8 +1,11 @@
 package io.github.salepartido.api.devtools.canchas;
 
 import java.time.DayOfWeek;
+import java.time.Duration;
+import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Locale;
 
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
@@ -22,32 +25,47 @@ public class SeedService {
     }
     */
 
-    public Collection<ConfiguracionDia> seed() {
+    public Collection<ConfiguracionDia> seed(int count) {
 
         Collection<ConfiguracionDia> configuracionesDias = new ArrayList<>();
 
-        Faker faker = new Faker();
-        for (int i = 0; i < 20; i++){
+        Faker faker = new Faker(Locale.of("es"));
+
+        for (int i = 0; i < count; i++) {
 
             ConfiguracionHorario horario = new ConfiguracionHorario();
-            horario.setCodigo(null);
+            horario.setCodigo(String.valueOf(i));
             horario.setActivo(true);
-            horario.setDuracionTurno(null);
-            horario.setCancha(null);
+            horario.setDuracionTurno(
+                    faker.options().option(
+                        Duration.ofMinutes(30),
+                        Duration.ofMinutes(60),
+                        Duration.ofMinutes(90),
+                        Duration.ofMinutes(120)
+                    )
+            );
 
             for (DayOfWeek dayOfWeek : DayOfWeek.values()) {
+
+                int horaInicio = faker.number().numberBetween(8, 18);
+
+                // duración del horario total
+                int horasDisponibles = faker.number().numberBetween(2, 6);
+
+                LocalTime inicio = LocalTime.of(horaInicio, 0);
+                LocalTime fin = inicio.plusHours(horasDisponibles);
+
                 ConfiguracionDia dia = new ConfiguracionDia();
+
                 dia.setDiaSemana(dayOfWeek);
-                dia.setHoraInicio(null);
-                dia.setHoraFin(null);
-                dia.setConfiguracionHorario(null);
+                dia.setHoraInicio(inicio);
+                dia.setHoraFin(fin);
                 dia.setConfiguracionHorario(horario);
 
-                // hacer save en vez de return
                 configuracionesDias.add(dia);
             }
         }
-        
+
         return configuracionesDias;
     }
 }
