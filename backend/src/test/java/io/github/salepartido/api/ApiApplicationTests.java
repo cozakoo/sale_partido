b173fd4114f28f92d3b89dca38d18ce16896edcd
cozakoo <martinarcosvargas2@gmail.com>
2026-05-14 13:40:55 -0300

    update de mockito

--- backend/src/test/java/io/github/salepartido/api/ApiApplicationTests.java
+++ backend/src/test/java/io/github/salepartido/api/ApiApplicationTests.java
@@ -2,13 +2,13 @@ package io.github.salepartido.api;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 @SpringBootTest
 class ApiApplicationTests {
 
-    @MockBean
+    @MockitoBean
     StringRedisTemplate redisTemplate;
 
     @Test
