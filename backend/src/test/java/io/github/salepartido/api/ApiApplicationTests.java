a400a389ebced80c97bbe8e5065c0f41ea51be39
cozakoo <martinarcosvargas2@gmail.com>
2026-05-14 13:38:23 -0300

    configurar entorno de tests con H2 y mock de Redis

--- backend/src/test/java/io/github/salepartido/api/ApiApplicationTests.java
+++ backend/src/test/java/io/github/salepartido/api/ApiApplicationTests.java
@@ -2,12 +2,17 @@ package io.github.salepartido.api;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.redis.core.StringRedisTemplate;
 
 @SpringBootTest
 class ApiApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @MockBean
+    StringRedisTemplate redisTemplate;
+
+    @Test
+    void contextLoads() {
+    }
 
 }
